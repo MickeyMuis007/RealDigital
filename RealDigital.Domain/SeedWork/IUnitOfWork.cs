4e79f644367a372cf5e51549ef7499f518cefe93
= <=>
2019-04-09 07:20:08 +0200

    Added Unit of work

--- RealDigital.Domain/SeedWork/IUnitOfWork.cs
+++ RealDigital.Domain/SeedWork/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RealDigital.Domain.AggregateModels.ContactAggregate;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace RealDigital.Domain.SeedWork
 {
     public interface IUnitOfWork
     {
-        
+        IContactRepository ContactRepository { get; }
     }
 }
--- RealDigital.Infrastructure/Implementations/Repositories/UnitOfWork.cs
+++ RealDigital.Infrastructure/Implementations/Repositories/UnitOfWork.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using RealDigital.Domain.AggregateModels.ContactAggregate;
+using RealDigital.Domain.SeedWork;
+using RealDigital.Infrastructure.Implementations.Repositories.ContactRepo;
+using RealDigital.Persistence.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +10,32 @@ using System.Threading.Tasks;
 
 namespace RealDigital.Infrastructure.Implementations.Repositories
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
-        
+        #region Fields
+
+        private readonly RealDigitalContext _context;
+        private IContactRepository _contactRepository;
+        #endregion Fields
+
+        #region Properties
+
+        public IContactRepository ContactRepository {
+            get
+            {
+                return _contactRepository = _contactRepository ?? new ContactRepository(_context);
+            }
+        }
+
+        #endregion Properties
+
+        #region Constructors 
+
+        public UnitOfWork(RealDigitalContext context)
+        {
+            _context = context;
+        }
+
+        #endregion Constructors
     }
 }
