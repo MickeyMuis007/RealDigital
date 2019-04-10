77ff731de2ad8650b82a25dc2895e4adab0759dc
= <=>
2019-04-10 07:56:26 +0200

    Displaying all contacts

--- RealDigital.UI/ClientApp/src/app/home/home.component.ts
+++ RealDigital.UI/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../models/contact.model';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private contactService: ContactService) {}
+
+
+  ngOnInit() {
+    this.contactService.getContacts().subscribe(
+      (res: Contact[]) => {
+        this.contacts = res;
+      },
+      err => { }
+    );
+  }
+
+  contacts: Contact[];
 }
